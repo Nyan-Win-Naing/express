5310c9f2c05a17b062c0f23e7b70820eed2324bb
nyan-win-naing-19 <s4115096@rmit.edu.vn>
2025-07-07 18:16:29 +0700

    EJS Components and Escape String

--- app.js
+++ app.js
@@ -1,33 +1,52 @@
 // console.log("app is running");
 
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
 app.set("views", "./views");
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.get('/', (req, res) => {
-    res.render('home');
+app.get("/", (req, res) => {
+  let blogs = [
+    // {
+    //   title: "Blog title update 1",
+    //   intro: "this is blog intro 1",
+    // },
+    {
+      title: "Blog title 2",
+      intro: "this is blog intro 2",
+    },
+    {
+      title: "Blog title 3",
+      intro: "this is blog intro 3",
+    },
+  ];
 
+  res.render("home", {
+    // name: "mgmg",
+    // age: 22,
+    // blogs: blogs,
+    blogs
+  });
 });
 
-app.get('/about', (req, res) => {
-    res.render("about");
+app.get("/about", (req, res) => {
+  res.render("about");
 });
 
 // app.get('/about-us', (req, res) => {
 //     res.redirect("/about");
 // });
 
-app.get('/contact', (req, res) => {
-    res.render("contact");
+app.get("/contact", (req, res) => {
+  res.render("contact");
 });
 
 app.use((req, res) => {
-    res.status(404).render("404");
+  res.status(404).render("404");
 });
 
 app.listen(3000, () => {
-    console.log("app is running on port 3000");
+  console.log("app is running on port 3000");
 });
