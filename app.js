bfd425784e8faeaed029630a40cbf98021b44981
nyan-win-naing-19 <s4115096@rmit.edu.vn>
2025-07-07 21:56:19 +0700

    Schema, Model and Data Create

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 // console.log("app is running");
 
 const express = require("express");
-let morgan = require("morgan")
+let morgan = require("morgan");
+const mongoose = require("mongoose");
+const Blog = require('./models/Blog');
 
 const app = express();
+
+// db url
+let mongoUrl =
+  "mongodb+srv://nyanwinnaing:test1234@cluster0.fimurck.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose
+  .connect(mongoUrl)
+  .then(() => {
+    console.log("connected to db");
+    app.listen(3000, () => {
+      console.log("app is running on port 3000");
+    });
+  })
+  .catch((e) => {
+    console.log(e);
+  });
 
 app.set("views", "./views");
 app.set("view engine", "ejs");
@@ -18,15 +36,25 @@ app.set("view engine", "ejs");
 //     if(env === 'dev') {
 //       console.log(`${req.method} ${req.originalUrl} --`);
 //     }
-      
+
 //     next();
 //   };
 // };
 
 // app.use(logger());
 
-app.use(morgan('dev'));
-app.use(express.static('public'));
+app.use(morgan("dev"));
+app.use(express.static("public"));
+
+app.get('/add-blog', async (req, res) => {
+  let blog = new Blog({
+    title: "blog title 2",
+    intro: "blog intro 2",
+    body: "blog body 2",
+  });
+  await blog.save();
+  res.send("blog saved");
+});
 
 app.get("/", (req, res) => {
   let blogs = [
@@ -73,8 +101,4 @@ app.use((req, res) => {
   res.status(404).render("404", {
     title: "404 Not Found",
   });
-});
-
-app.listen(3000, () => {
-  console.log("app is running on port 3000");
 });
